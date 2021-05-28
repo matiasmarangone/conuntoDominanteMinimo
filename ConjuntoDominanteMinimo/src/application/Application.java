cf58406ef7f4d69844860a5004e229660d94349a
Matias Marangone <matiasemarangone@gmail.com>
2021-05-27 21:18:04 -0300

    Started front end, added dinamic button on click

--- ConjuntoDominanteMinimo/src/application/Application.java
+++ ConjuntoDominanteMinimo/src/application/Application.java
@@ -2,19 +2,29 @@ package application;
 
 import java.awt.EventQueue;
 
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
-public class Application {
+public class Application extends JFrame implements ActionListener {
 
 	private JFrame frame;
 	JLayeredPane layeredPane = new JLayeredPane();
+	private JTextField textField;
+	int xPlacement = 36;
+	int yPlacement = 100;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -64,10 +74,37 @@ public class Application {
 		layeredPane.add(panelData);
 		panelData.setLayout(null);
 		
-		JLabel lblTestDataPanel = new JLabel("Panel Datos");
-		lblTestDataPanel.setBounds(287, 154, 217, 14);
-		lblTestDataPanel.setHorizontalAlignment(SwingConstants.CENTER);
-		panelData.add(lblTestDataPanel);
+		JLabel lblAddVertex = new JLabel("Cuantos vertices tendra su grafo?");
+		lblAddVertex.setBounds(36, 15, 312, 14);
+		panelData.add(lblAddVertex);
+		
+		textField = new JTextField();
+		textField.setBounds(332, 12, 86, 20);
+		panelData.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnAddVertex = new JButton("Agregar");
+		btnAddVertex.setBounds(446, 11, 89, 23);
+		panelData.add(btnAddVertex);
+		
+		JButton btnAddEdge = new JButton("Agregar arista");
+		btnAddEdge.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				  JButton btnEdgeInput = new JButton("Button added");
+				  //btnEdgeInput.setSize(150, 150);
+				
+				  btnEdgeInput.setBounds(xPlacement, yPlacement, 100, 20);
+				  yPlacement += 40;
+				  panelData.add(btnEdgeInput); //panelData.repaint(); panelData.validate();
+				  panelData.repaint();
+				  
+				
+				
+			}
+		});
+		btnAddEdge.setBounds(37, 51, 133, 23);
+		panelData.add(btnAddEdge);
 		
 		JPanel panelResults = new JPanel();
 		panelResults.setBounds(0, 0, 764, 473);
@@ -98,7 +135,10 @@ public class Application {
 		
 		
 	}
-	
 
-
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
